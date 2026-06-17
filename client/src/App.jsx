f0723557f065b13a996723834886dccd2cf6e3f3
Anya Singh <anyas4841@gmail.com>
2026-06-17 20:56:25 +0530

    Initial Focus Arena MVP2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,13 +17,3 @@
   0% {
     text-shadow: 0 0 15px #00ff9d;
   }
-  50% {
-    text-shadow:
-      0 0 40px #00ff9d,
-      0 0 80px #00d9ff;
-  }
-  100% {
-    text-shadow: 0 0 15px #00ff9d;
-  }
-}
-
